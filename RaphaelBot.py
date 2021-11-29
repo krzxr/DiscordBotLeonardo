11ecd2c17a90a091a47c360a06786bc5d6630334
krzxr <krong@g.hmc.edu>
2021-11-29 05:31:54 -0800

    reading bot

--- RaphaelBot.py
+++ RaphaelBot.py
@@ -1,9 +1,9 @@
 import re
 import random
 import discord
-from functools import partial
+from collections import defaultdict
 with open('raphael_token.txt','r') as f:
-    DISCORD_TOKEN = f.readline().strip()
+    DISCORD_TOKEN = f.readline()
 
 class book:
     def __init__(self,title=None):
@@ -11,60 +11,68 @@ class book:
         with open(title+'.txt','r') as f:
             self.content = f.readlines()
         self.page = 0
-    def read():
+    def read(self):
         if 0<=self.page<len(self.content):
-            return self.content[self.page]
+            text = self.content[self.page]
+            if text != '\n':
+                text = text.replace('_______','\n\n')
+                return text.strip()
+            else:
+                return text+'\n'
         else:
             return 'Reading content out of bound'
-class Rphael(discord.Client):
-    def warm_start(self):
-        self.book2page = {}
-        with open('raphael_db.txt','r') as f:
-            book, page = f.readline().strip().split(',')
-            page = int(page)
-            self.book2page[book] = page
-        self.book = None
-    def read(self,page):
-
+class Raphael(discord.Client):
+    book = defaultdict(lambda: None) 
     def respond(self,content,name):
-        reply = ''
+        reply = None
         arr = content.strip().split()
-        if self.book == None:
+        
+        if self.book[name] == None:
             if len(arr)==2 and arr[0].lower() == 'read':
                 book_name = arr[1].lower()
                 try:
-                    self.book = book(book_name)
+                    self.book[name] = book(book_name)
                     reply = 'Finish loading'
                 except OSError:
                     reply = 'File not found'
-                else:
-                    print('File reading error')
         else: 
             if len(arr)==1:
                 command = arr[0]
                 if command in ['i','n','inc','next']:
-                    reply = self.book.read()
-                    self.book.page += 1
+                    reply = self.book[name].read()
+                    self.book[name].page += 1
                 elif command in ['d']:
-                    self.book.page -= 2
-                    reply = self.book.read()
+                    self.book[name].page -= 2
+                    reply = self.book[name].read()
                 elif command in ['del']:
-                    self.book = None 
-
+                    self.book[name] = None 
+                    reply = 'Reset book cache'
+                elif command in ['name']:
+                    reply = self.book[name].title
+                elif command in ['page']:
+                    reply = self.book[name].page
             elif len(arr)==2:
                 command, num = arr
                 if command in ['split','j']:
+                    self.book[name].page = num
+                    reply = self.book[name].read()
+                elif command in ['i','n','inc','next']:
                     num = int(num)
-                    self.book.page = num
-                    reply = self.book.read()
-        if not reply == '':
-            return 'What\' up!'
+                    reply = []
+                    for _ in range(num):
+                        reply.append(self.book[name].read())
+                        self.book[name].page += 1
+                    reply = ' '.join(reply)
+
+        if reply == None:
+            return 'What\'s up!'
+        if reply == '':
+            reply = ' ' 
         return reply
         
     async def on_ready(self):
         # Runs when successfully connected to the server
         print('Logged on as', self.user)
-        self.warm_start()
     
     async def on_message(self, message):
         # don't respond to ourselves
